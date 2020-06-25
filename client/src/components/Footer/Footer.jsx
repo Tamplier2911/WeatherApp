b4ce90d0e873cfd6f7ef935265060bc97ed23527
Tamplier2911 <44438728+Tamplier2911@users.noreply.github.com>
2020-06-26 00:20:16 +0300

    Customized client styles in order them to match back-end templates.

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => (
   <footer className="footer">
-    <div>footer</div>
+    <div>Created by Artem Nikolaiev | &copy; 2020 All Rights Reserved</div>
   </footer>
 );
 
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import "./Header.scss";
 import React from "react";
 
+// img
+import logoImage from "../../assets/png/cloud_logo.png";
+
 const Header = () => (
   <header className="header">
-    <div>header</div>
+    <div>
+      <div className="header__imgwrap">
+        <img className="header__img" src={logoImage} alt="cloudy logo" />
+      </div>
+    </div>
   </header>
 );
 
